7d58c40f9174924f141b3b4d0560021bc210be4e
AlexU2001 <alexander_urenacollado@bloomfield.edu>
2021-03-03 21:15:47 -0500

    Unit 6

--- app/src/main/java/com/example/insta/LoginActivity.java
+++ app/src/main/java/com/example/insta/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,17 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username,password);
+            }
+        });
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
@@ -57,6 +70,27 @@ public class LoginActivity extends AppCompatActivity {
                 // Navigate to main activity if the user logs in properly
                 goMainActivity();
                 Toast.makeText(LoginActivity.this,"Successfully logged in", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void signupUser(String username, String password){
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    loginUser(username, password);
+                    Toast.makeText(LoginActivity.this, "Signed Up", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    Toast.makeText(LoginActivity.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                    // to figure out what went wrong
+                }
             }
         });
     }
